d084d0bfffc953738a1191329bc3eeebcc170841
Davis Akard <dbakard@hitmonlee0523.utk.tennessee.edu>
2025-06-03 11:46:48 -0400

    finished section 5.2

--- Git/Python/libwyag.py
+++ Git/Python/libwyag.py
@@ -204,6 +204,23 @@ def object_write(obj, repo=None):
                 f.write(zlib.compress(result))
         return sha
 
+def object_find(repo, name, fmt=None, follow=True):
+    return name
+
+def object_hash(fd, fmt, repo=None):
+    """ Hash object, writing it to repo if provided."""
+    data = fd.read()
+
+    # Choose constructor according to fmt argument
+    match fmt:
+        case b'commit'  : obj = GitCommit(data)
+        case b'tree'    : obj = GitTree(data)
+        case b'tag'     : obj = GitTag(data)
+        case b'blob'    : obj = GitBlob(data)
+        case _: raise Exception(f"Unknown type{fmt}!")
+
+    return object_write(obj, repo)
+
 # Class definition for git blobs
 class GitBlob(GitObject):
     fmt=b'blob'
@@ -214,16 +231,133 @@ class GitBlob(GitObject):
     def deserialize(self, data):
         self.blobdata = data
 
+# Cat functions
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+
+# Hash functions
+def cmd_hashobject(args):
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+# Commit parsing functions
+def kvlm_parse(raw, start=0, dct=None):
+    if not dct:
+        dct = dict()
+
+    spc = raw.find(b' ', start)
+    nl = raw.find(b'\n', start)
+
+    # Base case
+    # If newline appears first (or there's no space at all, in which case find returns -1),
+    # we assume a blank line. A blank line means the remainder of the data is the message.
+    # We store it in the dictionary, with None as the key, and return.
+    if (spc < 0) or (nl < spc):
+        assert nl == start
+        dct[None] = raw[start+1:]
+        return dct
+
+    # Recursive case
+    # We read a key-value pair and recurse for the next.
+    key = raw[start:spc]
+
+    # Find the end of the value. Continuation lines begin with a space, so we loop until we find a "\n" not followed by a space.
+    end = start
+    while True:
+        end = raw.find(b'\n', end+1)
+        if raw[end+1] != ord(' '): break
+
+    # Grab the value. Also, drop the leading space on continuation lines.
+    value = raw[spc+1:end].replace(b'\n', b'\n')
+
+    # Don't overwrite existing data contents.
+    if key in dct:
+        if type(dct[key]) == list:
+            dct[key].append(value)
+        else:
+            dct[key] = [ dct[key], value ]
+    else:
+        dct[key] = value
+
+    return kvlm_parse(raw, start=end+1, dct=dct)
+
+def kvlm_serialize(kvlm):
+    ret = b''
+
+    # Output fields
+    for k in kvlm.keys():
+        # Skip the message itself
+        if k == None: continue
+        val = kvlm[k]
+        # Normalize to a list
+        if type(val) != list:
+            val = [ val ]
+
+        for v in val:
+            ret += k + b' ' + (v.replace(b'\n', b'\n ')) + b'\n'
+
+        # Append message
+        ret += b'\n' + kvlm[None]
+
+        return ret
+
+# Class definition for git commits
+class GitCommit(GitObject):
+    fmt=b'commit'
+
+    def deserialize(self, data):
+        self.kvlm = kvlm_parse(data)
+
+    def serialize(self):
+        return kvlm_serialize(self.kvlm)
+
+    def init(self):
+        self.kvlm = dict()
+
 # Initialize argparsers
 argparser = argparse.ArgumentParser(description="The stupidest content tracker")
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
+
 argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
 argsp.add_argument("path",
                    metavar="directory",
                    nargs="?",
                    default=".",
                    help="Where to create the repository.")
+
+argsp = argsubparsers.add_parser("cat-file", help="Provide content of repository objects")
+argsp.add_argument("type",
+                   metavar="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   help="Specify the type")
+argsp.add_argument("object",i
+                   metavar="object",
+                   help="The object to display")
+
+argsp = argsubparsers.add_parser("hash-object", help="Compute object ID and optionally creates a blob from a file")
+argsp.add_argument("-t",
+                   metavar="type",
+                   dest="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   default="blob",
+                   help = "Specify the type")
+argsp.add_argument("-w",
+                   dest="write",
+                   action="store_true",
+                   help="Actually write the object into the database")
+argsp.add_argument("path", help="Read object from <file>")
 
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
