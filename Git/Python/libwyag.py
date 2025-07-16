8ca627abfb880f5ae5c1c228665cd71018501330
Davis Akard <dbakard@hitmonlee0523.utk.tennessee.edu>
2025-07-16 16:20:20 -0400

    finished up to section 8.5

--- Git/Python/libwyag.py
+++ Git/Python/libwyag.py
@@ -1,4 +1,3 @@
-# Import required libraries
 import argparse
 import configparser
 from datetime import datetime
@@ -454,7 +453,7 @@ class GitTreeLeaf (object):
 def tree_parse_one(raw, start=0):
     # Find the space terminator of the mode
     x = raw.find(b' ', start)
-    asser x-start == 5 or x-start==6
+    assert x-start == 5 or x-start==6
 
     # Read the mode
     mode = raw[start:x]
@@ -557,7 +556,7 @@ def cmd_checkout(args):
         if not os.path.isdir(args.path):
             raise Exception(f"Not a directory {args.path}!")
         if os.listdir(args.path):
-            raise Exception(f"Not empty {args.path!")
+            raise Exception(f"Not empty {args.path}!")
     else:
         os.makedirs(args.path)
 
@@ -669,6 +668,273 @@ def cmd_rev_parse(args):
 
     print (object_find(repo, args.name, fmt, follow=True))
 
+# Class definition for Git Index Entries
+class GitIndexEntry (object):
+    def __init__(self, ctime=None, mtime=None, dev=None, ino=None,
+                 mode_type=None, mode_perms=None, uid=None, gid=None,
+                 fsize=None, sha=None, flag_assume_valid=None,
+                 flag_stage=None, name=None):
+        # The last time a file's metadata changed. (timestamp in seconds, nanoseconds)
+        self.ctime = ctime
+        # The last time a file's data changed. (timestamp in seconds, nanoseconds)
+        self.mtime = mtime
+        # The ID of device containing this file
+        self.dev = dev
+        # The file's inode number
+        self.ino = ino
+        # The object type, either b1000 (regular), b1010 (symlink), b1110 (gitlink)
+        self.mode_type = mode_type
+        # The object permissions, an integer
+        self.mode_perms = mode_perms
+        # User ID of owner
+        self.uid = uid
+        # Group ID of owner
+        self.gid = gid
+        # Size of this object, in bytes
+        self.fsize = fsize
+        # The object's SHA
+        self.sha = sha
+        self.flag_assume_valid = flag_assume_valid
+        self.flag_stage = flag_stage
+        # Name of the object (full path)
+        self.name = name
+
+# Class definition for Git Indices
+class GitIndex (object):
+    version = None
+    entries = []
+    # ext = None
+    # sha = None
+
+    def __init__(self, version=2, entries=None):
+        if not entries:
+            entries = List()
+
+        self.version = version
+        self.entries = entries
+
+# Index functions
+def index_read(repo):
+    index_file = repo_file(repo, "index")
+
+    # New repos have no index!
+    if not os.path.exists(index_file):
+        return GitIndex()
+
+    with open(index_file, 'rb') as f:
+        raw = f.read()
+
+    header = raw[:12]
+    signature = header[:4]
+    assert signature == b"DIRC" # Stands for "DirCache"
+    version = int.from_bytes(header[4:8], "big")
+    assert version == 2, "wyag only supports index file version 2"
+    count = int.from_bytes(header[8:12], "big")
+
+    entries = list()
+
+    content = raw[12:]
+    idx = 0
+    for i in range(0, count):
+        # Read creation time, as a unix timestamp
+        ctime_s = int.from_bytes(content[idx: idx+4], "big")
+        # Read creation time, as nanoseconds after that timestamp
+        ctime_ns = int.from_bytes(content[idx+4: idx+8], "big")
+        # Same for modification time
+        mtime_s = int.from_bytes(content[idx+8: idx+12], "big")
+        mtime_ns = int.from_bytes(content[idx+12: idx+16], "big")
+        dev = int.from_bytes(content[idx+16: idx+20], "big")
+        ino = int.from_bytes(content[idx+20: idx+24], "big")
+        # Ignored.
+        unused = int.from_bytes(content[idx+24: idx+26], "big")
+        assert 0 == unused
+        mode = int.from_bytes(content[idx+26: idx+28], "big")
+        mode_type = mode >> 12
+        assert mode_type in [0b1000, 0b1010, 0b1110]
+        mode_perms = mode & 0b0000000111111111
+        uid = int.from_bytes(content[idx+28: idx+32], "big")
+        gid = int.from_bytes(content[idx+32: idx+36], "big")
+        fsize = int.from_bytes(content[idx+36: idx+40], "big")
+        # SHA (object ID), stored as lowercase hex string
+        sha = format(int.from_bytes(content[idx+60: idx+62], "big"), "040x")
+        # Parse flags
+        flag_assume_valid = (flags & 0b1000000000000000) != 0
+        flag_extended = (flags & 0b0100000000000000) != 0
+        assert not flag_extended
+        flag_stage = flags & 0b0011000000000000
+        # Length of the name. This is stored on 12 bits, some max
+        # value is 0xFFF, 4095. Since names can occasionally go
+        # beyond that length, git teats 0xFFF as meaning at least
+        # 0xFFF, and looks for the final 0x00 to find the end of the
+        # name -- at a small, and probably very rare, performance cost.
+        name_length = flags & 0b0000111111111111
+        
+        # We've read 62 bytes so far.
+        idx += 62
+
+        if name_length < 0xFFF:
+            assert content[idx + name_length] == 0x00
+            raw_name = content[idx:idx+name_length]
+            idx += name_length + 1
+        else:
+            print(f"Notice: Name is 0x{name_length:X} bytes long.")
+            null_idx = content.find(b'\x00', idx + 0xFFF)
+            raw_name = content[idx: null_idx]
+            idx = null_idx + 1
+
+        # Just parse the name as utf8
+        name = raw_name.decode("utf8")
+
+        # Data is padded on multiples of eight bytes for pointer
+        # alignment, so we skip as many bytes as we need for the next
+        # read to start at the right position.
+
+        idx = 8 * ceil(idx / 8)
+
+        # And we add this entry to our list.
+        entries.append(GitIndexEntry(ctime=(ctime_s, ctime_ns),
+                                     mtime=(mtime_s, mtime_ns),
+                                     dev=dev,
+                                     ino=ino,
+                                     mode_type=mode_type,
+                                     mode_perms=mode_perms,
+                                     uid=uid,
+                                     gid=gid,
+                                     fsize=fsize,
+                                     sha=sha,
+                                     flag_assume_valid=flag_assume_valid,
+                                     flag_stage=flag_stage,
+                                     name=name))
+        
+        return GitIndex(version=version, entries=entries)
+
+# ls files functions
+def cmd_ls_files(args):
+    repo = repo_find()
+    index = index_read(repo)
+    if args.verbose:
+            print(f"Index file format v{index.version}, containing {len(index.entries)} entries.")
+
+    for e in index.entries:
+        print(e.name)
+        if args.verbose:
+            entry_type = { 0b1000: "regular file",
+                           0b1010: "symlink",
+                           0b1110: "git link" }[e.mode_type]
+            print(f"  {entry_type} with perms: {e.mode_perms:o}")
+            print(f"  on blob: {e.sha}")
+            print(f"  created: {datetime.fromtimestamp(e.ctime[0])}.{e.ctime[1]}, modified: {datetime.fromtimestamp(e.mtime[0])}.{e.mtime[1]}")
+            print(f"  device: {e.dev}, inode: {e.ino}")
+            print(f"  user: {pwd.getpwuid(e.uid).pw_name} ({e.uid}) group: {grp.getgrgid(e.gid).gr_name} ({e.gid})")
+            print(f"  flags: stag={e.flag_stage} assume_valid={e.flag_assume_valid}")
+
+# Class definition for Git Ignore
+class GitIgnore(object):
+    absolute = None
+    scoped = None
+
+    def __init__(self, absolute, scoped):
+        self.absolute = absolute
+        self.scoped = scoped
+
+# Check ignore functions
+def cmd_check_ignore(args):
+    repo = repo_find()
+    rules = gitignore_read(repo)
+    for path in args.path:
+        if check_ignore(rules, path):
+            print(path)
+
+def gitignore_parse1(raw):
+    raw = raw.strip() # Remove leading/trailing spaces
+
+    if not raw or raw[0] == "#":
+        return None
+    elif raw[0] == "!":
+        return (raw[1:], False)
+    elif raw[0] == "\\":
+        return (raw[1:], True)
+    else:
+        return (raw, True)
+
+def gitignore_parse(lines):
+    ret = list()
+
+    for line in lines:
+        parsed = gitignore_parse1(line)
+        if parsed:
+            ret.append(parsed)
+
+    return ret
+
+def gitignore_read(repo):
+    ret = GitIgnore(absolute=list(), scoped=dict())
+
+    # Read local configuration in .git/info/exclude
+    repo_file = os.path.join(repo.gitdir, "info/exclude")
+    if os.path.exists(repo_file):
+        with open(repo_file, "r") as f:
+            ret.absolute.append(gitignore_parse(f.readlines()))
+
+    # Global configuration
+    if "XDG_CONFIG_HOME" in os.environ:
+        config_home = os.environ["XDG_CONFIG_HOME"]
+    else:
+        config_home = os.path.expanduser("~/.config")
+    global_file = os.path.join(config_home, "git/ignore")
+
+    if os.path.exists(global_file):
+        with open(global_file, "r") as f:
+            ret.absolute.append(gitignore_parse(f.readlines()))
+
+    # .gitignore files in the index
+    index = index_read(repo)
+
+    for entry in index.entries:
+        if entry.name == ".gitignore" or entry.name.endswith("/.gitignore"):
+            dir_name = os.path.dirname(entry.name)
+            contents = object.read(repo, entry.sha)
+            lines = contents.blobdata.decode("utf8").splitlines()
+            ret.scoped[dir_name] = gitignore_parse(lines)
+    return ret
+
+def check_ignore1(rules,path):
+    result = None
+    for (pattern, value) in rules:
+        if fnmatch(path, pattern):
+            result = value
+    return result
+
+def check_ignore_scoped(rules, path):
+    parent = os.path.dirname(path)
+    while True:
+        if parent in rules:
+            result = check_ignore1(rules[parent], path)
+            if result != None:
+                return result
+        if parent == "":
+            break
+        parent = os.path.dirname(parent)
+    return None
+
+def check_ignore_absolute(rules, path):
+    parent = os.path.dirname(path)
+    for ruleset in rules:
+        result = check_ignore1(ruleset, path)
+        if result != None:
+            return result
+    return False # This is a reasonable default at this point.
+
+def check_ignore(rules, path):
+    if os.path.isabs(path):
+        raise Exception("This function requires path to be relative to the repository's root")
+
+    result = check_ignore_scoped(rules.scoped, path)
+    if result != None:
+        return result
+
+    return check_ignore_absolute(rules.absolute, path)
+
 # Initialize argparsers
 argparser = argparse.ArgumentParser(description="The stupidest content tracker")
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
@@ -686,7 +952,7 @@ argsp.add_argument("type",
                    metavar="type",
                    choices=["blob", "commit", "tag", "tree"],
                    help="Specify the type")
-argsp.add_argument("object",i
+argsp.add_argument("object",
                    metavar="object",
                    help="The object to display")
 
@@ -744,6 +1010,12 @@ argsp.add_argument("--wyag-type",
                    default=None,
                    help="Specify the expected type")
 argsp.add_argument("name", help="The name to parse")
+
+argsp = argsubparsers.add_parser("ls-files", help = "List all the stage files")
+argsp.add_argument("--verbose", action="store_true", help="Show everything.")
+
+argsp = argsubparsers.add_parser("check-ignore", help = "Check path(s) against ignore rules.")
+argsp.add_argument("path", nargs="+", help="Paths to check")
 
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
