a0e8b8cd2ad8388908fdf382ade6dd1b194acf35
Davis Akard <davisakard@gmail.com>
2025-07-23 15:47:21 -0400

    finished chapter 9

--- Lisp/C/parsing.c
+++ Lisp/C/parsing.c
@@ -47,6 +47,10 @@ enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
 void lval_print(lval* v);
 void lval_expr_print(lval* v, char open, char close);
 lval* lval_add(lval* v, lval* x);
+lval* lval_eval(lval* v);
+lval* lval_take(lval* v, int i);
+lval* lval_pop(lval* v, int i);
+lval* builtin_op(lval* a, char* op);
 
 /* Create a new number type lval */
 lval* lval_num(long x) {
@@ -168,6 +172,102 @@ lval* lval_add(lval* v, lval* x) {
     return v;
 }
 
+lval* lval_eval_sexpr(lval* v) {
+    /* Evaluate Children */
+    for (int i = 0; i < v->count; i += 1) {
+        v->cell[i] = lval_eval(v->cell[i]);
+    }
+
+    /* Error Checking */
+    for (int i = 0; i < v->count; i += 1) {
+        if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+    }
+
+    /* Empty Expression */
+    if (v->count == 0) { return v; }
+
+    /* Single Expression */
+    if (v->count == 1) { return lval_take(v, 0); }
+
+    /* Ensure First Element is Symbol */
+    lval* f = lval_pop(v, 0);
+    if (f->type != LVAL_SYM) {
+        lval_del(f);
+        lval_del(v);
+        return lval_err("S-expression does not start with symbol!");
+    }
+
+    /* Call builtin with operator */
+    lval* result = builtin_op(v, f->sym);
+    lval_del(f);
+    return result;
+}
+
+lval* lval_eval(lval* v) {
+    if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+    return v;
+}
+
+lval* lval_pop(lval* v, int i) {
+    /* Find the item at "i" */
+    lval* x = v->cell[i];
+
+    /* Shift memory after the item at "i" over the top */
+    memmove(&v->cell[i], &v->cell[i+1], sizeof(lval*) * (v->count-i-1));
+
+    /* Decrease the count of items in the list */
+    v->count--;
+
+    /* Reallocate the memory used */
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    return x;
+}
+
+lval* lval_take(lval* v, int i) {
+    lval* x = lval_pop(v, i);
+    lval_del(v);
+    return x;
+}
+
+lval* builtin_op(lval* a, char* op) {
+    /* Ensure all arguments are numbers */
+    for (int i = 0; i < a->count; i += 1) {
+        if (a->cell[i]->type != LVAL_NUM) {
+            lval_del(a);
+            return lval_err("Cannot operate on non-number!");
+        }
+    }
+
+    /* Pop the first element */
+    lval* x = lval_pop(a, 0);
+
+    /* If no arguments and sub then perform unary negation */
+    if ((strcmp(op, "-") == 0) && a->count == 0) {
+        x->num = x->num;
+    }
+
+    /* While there are still elements remaining */
+    while (a->count > 0) {
+        /* Pop the next element */
+        lval* y = lval_pop(a, 0);
+
+        if (strcmp(op, "+") == 0) { x->num += y->num; }
+        if (strcmp(op, "-") == 0) { x->num -= y->num; }
+        if (strcmp(op, "*") == 0) { x->num *= y->num; }
+        if (strcmp(op, "/") == 0) {
+            if (y->num == 0) {
+                lval_del(x); lval_del(y);
+                x = lval_err("Division By Zero!"); 
+                break;
+            }
+            x->num /= y->num;
+        }
+        lval_del(y);
+    }
+    lval_del(a);
+    return x;
+}
+
 /* Use operator string to see which operation to perform */
 lval* eval_op(lval* x, char* op, lval* y) {
     /* If either value is an error return it */
@@ -240,7 +340,7 @@ int main(int argc, char** argv) {
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Lispy, &r)) {
 			/* On Success Print the Result */
-			lval* x = lval_read(r.output);
+			lval* x = lval_eval(lval_read(r.output));
 			lval_println(x);
 			lval_del(x);
 		} else {
